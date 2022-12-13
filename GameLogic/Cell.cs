2389e4f9fb27e4e214b99e9ab49af62e0ed9ff74
AndrewKhoryakov <andrejhorakov47232@gmail.com>
2022-12-13 22:54:57 +0300

    [add] Cell and Player

--- GameLogic/Cell.cs
+++ GameLogic/Cell.cs
@@ -2,5 +2,25 @@ namespace GameLogic;
 
 public class Cell
 {
-    
+    private int _countPoints;
+    private Player _owner;
+    public int CountPoints {
+        get
+        {
+            return this._countPoints; 
+        } 
+    }
+    public Player Owner
+    {
+        get
+        {
+            return this._owner;
+        }
+    }
+
+    public void AddCountPoints() => _countPoint = _countPoints++;
+
+    public void ResetCountPoints() => _countPoints = 0;
+
+    public void ChangeOwner(Player owner) => _owner = owner;
 }
--- GameLogic/Player.cs
+++ GameLogic/Player.cs
@@ -2,5 +2,8 @@ namespace GameLogic;
 
 public class Player
 {
+    public string Name { get; set; }
+    public readonly Color Color { get; set; }
     
+
 }
