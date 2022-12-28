31dd0c1502288299d0b66fdc1c546e16731fd158
scilightener <nikita.l.boss@gmail.com>
2022-12-28 23:18:18 +0300

    [add] missed files

---
+++ CollapseGameFormsApp/Form1Helpers.cs
@@ -0,0 +1,22 @@
+﻿namespace CollapseGameFormsApp
+{
+    public partial class Form1 : Form
+    {
+        [Flags]
+        enum AnimateWindowFlags
+        {
+            AW_HOR_POSITIVE = 0x00000001,
+            AW_HOR_NEGATIVE = 0x00000002,
+            AW_VER_POSITIVE = 0x00000004,
+            AW_VER_NEGATIVE = 0x00000008,
+            AW_CENTER = 0x00000010,
+            AW_HIDE = 0x00010000,
+            AW_ACTIVATE = 0x00020000,
+            AW_SLIDE = 0x00040000,
+            AW_BLEND = 0x00080000
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool AnimateWindow(IntPtr hWnd, int time, AnimateWindowFlags flags);
+    }
+}
